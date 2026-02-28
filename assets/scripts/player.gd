9840d8435d0d8ecd7882b53492fe8ebacaf8050c
Yassin-Said <said.yassin@epitech.eu>
2026-02-28 10:56:09 +0100

    [FIX] Attack fixed

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var attack_area: Area2D = $AttackArea
 @onready var collision_shape_2d: CollisionShape2D = $AttackArea/CollisionShape2D
+@onready var attack_area: CollisionShape2D = $AttackArea/CollisionShape2D
 const SPEED = 100.0
 const SPEED_IN_JUMP = 10.0
 const JUMP_VELOCITY = -200.0
@@ -66,7 +66,7 @@ func check_direction(direction: float, delta: float):
 
 func _physics_process(delta: float) -> void:
 	cooldown_wall -= delta
-	var direction := Input.get_axis("left", "right")
+	var direction := Input.get_axis("move_left", "move_right")
 	
 	if not is_on_floor():
 		if not holding or not direction:
@@ -82,31 +82,34 @@ func _physics_process(delta: float) -> void:
 			animated_sprite_2d.play("jump")
 		elif not is_on_floor() and is_on_wall():
 			jump_from_wall()
-  attack()
+	attack()
 	check_wall_collision(delta, direction)
 	check_direction(direction, delta)
 	move_and_slide()
 
 func attack():
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_just_pressed("attack") and not is_attacking:
 		is_attacking = true
 		var direction := Input.get_axis("ui_left", "ui_right")
-		if direction != 0:
-			animated_sprite_2d.flip_h = direction < 0
+		if direction < 0:
+			animated_sprite_2d.flip_h = true
+			attack_area.position.x = -33
+		elif direction > 0:
+			animated_sprite_2d.flip_h = false
+			attack_area.position.x = 33
 		animated_sprite_2d.play("attack")
-		collision_shape_2d.disabled = false
-
-func _on_animated_sprite_2d_animation_finished():
-	if animated_sprite_2d.animation == "attack":
-		is_attacking = false
-		collision_shape_2d.disabled = true
+		attack_area.disabled = false
 
 func _on_attack_area_body_entered(body: Node2D) -> void:
 	print("you hit something")
 	#if body.is_in_group("Enemies"):
 		#body.take_damage()
 	pass # Replace with function body.
+
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if animated_sprite_2d.animation == "jump" or animated_sprite_2d.animation == "wall_jump":
 		animated_sprite_2d.play("fall_loop")
+	if animated_sprite_2d.animation == "attack":
+		is_attacking = false
+		collision_shape_2d.disabled = true
 	
