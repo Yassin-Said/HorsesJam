a21c7f291de8426762b29e1f47fbca07f15a61ea
Deathm12 <jerome.paton@epitech.eu>
2026-03-01 20:42:36 +0400

    [FIX] - Fix wall slide and [ADD] add PlayerUi

--- assets/scripts/PlayerUi.gd
+++ assets/scripts/PlayerUi.gd
@@ -9,16 +9,15 @@ var is_low_time := false
 var penalty_tween: Tween
 
 @export var max_dashes: int = 3
-@export var dash_recharge_time: float = 2.0
+@export var dash_recharge_time: float = 1.5
 
 var current_dashes: int
-#var dash_cooldowns: Array[float] = []
 var recharge_timer: float = 0.0
 var recharge_queue: int = 0
 
 @onready var timer_label: RichTextLabel = $CanvasLayer/Control/TimerLabel
 @onready var penalty_label: RichTextLabel = $CanvasLayer/TimePenaltyLabel
-@onready var dash_container = $DashContainer
+@onready var dash_container: HBoxContainer = $CanvasLayer/DashContainer
 @onready var control: Control = $CanvasLayer/Control
 
 var original_position
@@ -36,15 +35,15 @@ func _ready():
 func _process(delta):
 	update_timer(delta)
 	update_dash_recharge(delta)
-	if Input.is_action_just_pressed("attack"):
-		apply_time_penalty()
-	if Input.is_action_just_pressed("jump"):
-		use_dash()
+	#if Input.is_action_just_pressed("attack"):
+		#apply_time_penalty()
+	#if Input.is_action_just_pressed("jump"):
+		#use_dash()
 
 func update_timer(delta):
 	if current_time <= 0:
 		current_time = 0
-		print("gars na pu le temps")
+		#print("gars na pu le temps")
 		return
 	
 	current_time -= delta
@@ -71,7 +70,7 @@ func apply_time_penalty(amount: float = hit_time_penalty):
 	if current_time < 0:
 		current_time = 0
 		update_timer_display()
-		print("gars na pu le temps parce que ou la touche un zafaire te dangereux")
+		#print("gars na pu le temps parce que ou la touche un zafaire te dangereux")
 	show_penalty(amount)
 
 func show_penalty(amount: float):
--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -6,42 +6,48 @@ extends CharacterBody2D
 @onready var attack_area: CollisionShape2D = $AttackArea/CollisionShape2D
 @onready var animation_player: AnimatedSprite2D = $AnimatedSprite2D
 @onready var player_sprite: Sprite2D = $Sprite2D
+@onready var player_ui: Control = $"../PlayerUi"
 
 const SPEED = 100.0
 const SPEED_IN_JUMP = 10.0
 const JUMP_VELOCITY = -200.0
 
 var fliped = false
-var land_on_wall = false
 var jumped_from_wall = false
 var jumping = false
 var sliding = false
 var current_dashing_key = ""
 var sliding_speed = 0
-var holding = false
 var cooldown_wall : float = 0
 var is_attacking = false
 var key_history = {"move_left": 0, "move_right": 0, "dash": 0}
 var dash_direction = Vector2.ZERO
 var current_key = ""
 var dash_speed = 500.0
-var dash_duration = 0.05
+var dash_duration = 0.1
 var dash_timer = 0.0
 var reset_key = 0
 var dashing = false
+var double_jump = false
+
+var wall_sliding = false
+var wall_slide_speed = 40.0
+var last_wall_dir = 0
+
+var base_sprite_x = 0.0
 
 func _ready() -> void:
+	base_sprite_x = animation_player.position.x
 	animation_player.play("idle")
 
-func jump_from_wall():
-	if holding:
-		cooldown_wall = 0.5
-		velocity = Vector2(100 * get_wall_normal().x, -300)
-		if holding == true:
-			reset_player_pos()
-		holding = false
-		jumped_from_wall = true
-		animation_player.play("wall_jump")
+func wall_jump():
+	var wall_dir = get_wall_normal().x
+	jumped_from_wall = true
+	wall_sliding = false
+	velocity.x = wall_dir * 100
+	velocity.y = -300
+	animation_player.play("wall_jump")
+	cooldown_wall = 0.2
 
 func flip_collision(direction):
 	if direction > 0:
@@ -61,48 +67,44 @@ func reset_player_pos():
 	else:
 		animation_player.position.x += 5
 
-func check_wall_collision(delta: float, direction: float):
-	if direction:
-		if is_on_wall() and not is_on_floor() and cooldown_wall <= 0:
-			if (is_on_wall() and get_wall_normal().x > 0 and direction < 0) or (is_on_wall() and get_wall_normal().x < 0 and direction > 0):
-				if not holding:
-					if get_wall_normal().x < 0:
-						animation_player.position.x += 3
-					else:
-						animation_player.position.x -= 5
-				holding = true
-				jumping = false
-			else:
-				return
+func check_wall_slide(direction):
+	wall_sliding = false
+
+	if is_on_wall() and not is_on_floor() and velocity.y > 0:
+
+		var wall_dir = get_wall_normal().x
+
+		if direction == -wall_dir:
+			wall_sliding = true
+			last_wall_dir = wall_dir
 			jumped_from_wall = false
-			if not land_on_wall:
-				velocity.y = 0
-				land_on_wall = true
-			velocity.y += 200 * delta
+			
+			velocity.y = min(velocity.y, wall_slide_speed)
+			
 			animation_player.play("wall_slide")
-			flip_collision(get_wall_normal().x)
-			if get_wall_normal().x < 0:
-				#animation_player.position.x = ray_point.x
-				animation_player.flip_h = true
+			animation_player.flip_h = wall_dir < 0
+
+			if wall_dir < 0:
+				animation_player.position.x = base_sprite_x + 3
 			else:
-				#animation_player.position.x = ray_point.x
-				animation_player.flip_h = false
-		else:
-			land_on_wall = false
+				animation_player.position.x = base_sprite_x - 5
+
+	if not wall_sliding:
+		animation_player.position.x = base_sprite_x
 
 func check_fall():
-	if not is_on_floor() and not holding and not jumped_from_wall and not jumping and not is_attacking:
+	if not is_on_floor() and not jumped_from_wall and not jumping and not is_attacking:
 		animation_player.play("fall_loop")
 
-func check_direction(direction: float, delta: float):
+func check_direction(direction: float):
 	if direction:
-		if not holding and not jumped_from_wall and not sliding:
+		if not jumped_from_wall and not sliding and not wall_sliding:
 			flip_collision(direction)
 			if direction < 0 :
 				animation_player.flip_h = true
 			else:
 				animation_player.flip_h = false
-		if holding == true or sliding == true or dashing == true or is_attacking == true:
+		if sliding == true or dashing == true or is_attacking == true:
 			return
 		if direction < 0 and is_on_floor():
 			if not jumping:
@@ -112,18 +114,13 @@ func check_direction(direction: float, delta: float):
 				animation_player.play("walk")
 		if not jumped_from_wall:
 			velocity.x = direction * SPEED
-		#else:
-			#velocity.x = direction * SPEED_IN_JUMP
 	else:
-		if holding == true:
-			reset_player_pos()
-		holding = false
 		if not is_on_wall() and not jumped_from_wall:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			if jumping == false and not is_attacking and not sliding:
 				animation_player.play("idle")
 
-func check_slide(direction, delta):
+func check_slide(delta):
 	if is_on_floor() and not sliding and not is_attacking:
 		if Input.is_action_just_pressed("slide"):
 			sliding_speed = 200
@@ -148,9 +145,6 @@ func clear_dash():
 		key_history[key] = 0
 
 func start_dash(dir: Vector2):
-	if holding == true:
-		reset_player_pos()
-	holding = false
 	jumped_from_wall = false
 	dash_direction = dir.normalized()
 	dash_timer = dash_duration
@@ -160,7 +154,7 @@ func apply_dash(delta):
 	if dashing:
 		dash_timer -= delta
 		
-		if current_dashing_key == "jump":
+		if current_dashing_key == "dash":
 			velocity = dash_direction * 250
 		else:
 			velocity = dash_direction * dash_speed
@@ -171,9 +165,9 @@ func check_dash(delta):
 	reset_key -= delta
 	if reset_key <= 0:
 		clear_dash()
-	if not is_on_floor() and not holding and not is_attacking:
+	if not is_on_floor() and not is_attacking:
 		for key in key_history:
-			if Input.is_action_just_pressed(key):
+			if Input.is_action_just_pressed(key) and player_ui.current_dashes > 0:
 				reset_key = 0.2
 				key_history[key] += 1
 			if key_history[key] >= 2:
@@ -185,7 +179,9 @@ func check_dash(delta):
 					animation_player.play("dash")
 					start_dash(Vector2.RIGHT)
 				if key == "dash":
+					animation_player.play("dash_up")
 					start_dash(Vector2.UP)
+				player_ui.use_dash()
 				dashing = true
 				clear_dash()
 	else:
@@ -196,26 +192,27 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	
 	if not is_on_floor():
-		if not holding:
-			velocity += get_gravity() * delta * 0.6
+		velocity += get_gravity() * delta * 0.6
 	else:
 		jumping= false
-		if holding == true:
-			reset_player_pos()
-		holding = false
+		double_jump = false
 		jumped_from_wall = false
 	if Input.is_action_just_pressed("jump") and not is_attacking:
-		if is_on_floor():
+		if is_on_floor() and not double_jump:
 			jumping = true
 			velocity.y = JUMP_VELOCITY
 			animation_player.play("jump")
-		elif not is_on_floor() and is_on_wall():
-			jump_from_wall()
+		elif wall_sliding:
+			wall_jump()
+		if not is_on_floor() and not is_on_wall() and not double_jump:
+			double_jump = true
+			velocity.y = JUMP_VELOCITY
+			animation_player.play("jump")
 	attack()
-	check_wall_collision(delta, direction)
-	check_direction(direction, delta)
+	check_wall_slide(direction)
+	check_direction(direction)
 	check_fall()
-	check_slide(direction, delta)
+	check_slide(delta)
 	check_dash(delta)
 	apply_dash(delta)
 	move_and_slide()
@@ -223,7 +220,6 @@ func _physics_process(delta: float) -> void:
 func attack():
 	if Input.is_action_just_pressed("attack") and not is_attacking:
 		is_attacking = true
-		var direction := Input.get_axis("ui_left", "ui_right")
 		if animation_player.flip_h == true:
 			attack_area.position.x = -33
 		elif animation_player.flip_h == false:
@@ -233,9 +229,6 @@ func attack():
 
 func _on_attack_area_body_entered(body: Node2D) -> void:
 	print("you hit something")
-	#if body.is_in_group("Enemies"):
-		#body.take_damage()
-	pass # Replace with function body.
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if animation_player.animation == "dash":
